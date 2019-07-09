a84eb0fbbf9a41bd9a1a6724fef1214cb86eacfa
Navvi24 <51583613+Navvi24@users.noreply.github.com>
2019-07-09 13:09:50 +0530

    Get LastInsertID and delete a row
    Delete a row from Table navdeep and also get last insert id

---
+++ Database_connectivity/deleteRow.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$pass = "";
+try
+{
+  $conn = new PDO("mysql:host=$servername;dbname=HelloDB",$username,$pass);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "delete from navdeep where name = 'deep'";
+  $conn->exec($sql);
+  echo "data deleted Successfully <br>";
+} catch (PDOException $e)
+{
+    echo "$sql" . "<br>" . $e->getMessage();
+}
+ ?>
---
+++ Database_connectivity/getlastID.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$pass = "";
+try
+{
+  $conn = new PDO("mysql:host=$servername;dbname=HelloDB",$username,$pass);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "insert into navdeep values(22,'deep','21')";
+  $conn->exec($sql);
+  $last_id = $conn->lastInsertId();
+  echo "New Record Inserted Successfully. Last Insert ID is :-  " . $last_id;
+}
+catch (PDOException $e)
+{
+  echo "$sql" . "<br>" . $e.getMessage();
+}
+$conn =  null;
+ ?>
